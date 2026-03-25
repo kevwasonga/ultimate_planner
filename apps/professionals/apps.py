b55f412d7ee6df61314cb1fffab4da1e8214f1e9
kevwasonga <kevinwasonga116@gmail.com>
2026-03-25 16:06:27 +0300

    apps: Update app configuration metadata

---
+++ apps/professionals/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class ProfessionalsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'professionals'
+
+
+    verbose_name = 'BuildConnect Professionals'
